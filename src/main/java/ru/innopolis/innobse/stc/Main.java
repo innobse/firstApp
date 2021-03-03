0b4b8e7939ff7e7dcda3a7137ec6c008d2006cf8
Yury Penkov <bsesie71@gmail.com>
2021-03-03 19:39:09 +0300

    change souts

--- src/main/java/ru/innopolis/innobse/stc/Main.java
+++ src/main/java/ru/innopolis/innobse/stc/Main.java
@@ -3,7 +3,9 @@ package ru.innopolis.innobse.stc;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("My first application!");
+    System.out.println("Yury");
+
+    System.out.println("Really! It's very fun!");
   }
 
 }
